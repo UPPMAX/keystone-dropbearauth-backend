e71da503f16fa4d7d913036cd81e49e552107738
Pontus Freyhult <pontus_github@soua.net>
2015-05-25 15:12:27 +0200

    Initial experiment.

---
+++ dropbearauth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+import subprocess
+import os
+from . import sql
+
+class Identity(sql.Identity):
+    def _check_password(self, password, user_ref):
+        username = user_ref.get('name')
+        
+        if (username in ['admin', 'nova', 'swift']):
+            return super(Identity, self)._check_password(password, user_ref)
+
+        os.putenv("DROPBEAR_PASSWORD",password)
+        exit = subprocess.call(["dbclient", "-l",username,"tintin.uppmax.uu.se","true"])        
+
+        if exit == 0: 
+          return True
+
+        return False
