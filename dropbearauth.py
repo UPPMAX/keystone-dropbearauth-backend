392b325ac989fa561bdc56245a524340310cf85b
Pontus Freyhult <pontus_git@soua.net>
2015-09-06 16:29:13 +0200

    Do not rely on dbclient exit status any more.

--- dropbearauth.py
+++ dropbearauth.py
@@ -10,20 +10,21 @@
 from __future__ import absolute_import
 import subprocess
 import os
+import uuid
 from . import sql
 
 class Identity(sql.Identity):
     def _check_password(self, password, user_ref):
         username = user_ref.get('name')
-        
+        s = uuid.uuid4().get_hex()
         if (username in ['admin', 'nova', 'swift']):
             return super(Identity, self)._check_password(password, user_ref)
 
         os.putenv("DROPBEAR_PASSWORD",password)
-        exit = subprocess.call(["dbclient", "-l",username,"tintin.uppmax.uu.se","true"])        
+        retstring = subprocess.check_output(["dbclient", "-l",username,"tintin.uppmax.uu.se","echo %s" % s])        
         os.unsetenv("DROPBEAR_PASSWORD")
 
-        if exit == 0: 
+        if retstring.find(s) != -1: 
           return True
 
         # Be nice at first!
